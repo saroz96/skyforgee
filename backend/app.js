44f84eaf6bfd7e54072b5f4db5aa8aaeaff7eb85
saroz96 <sarozbasnet97@gmail.com>
2025-08-04 14:55:43 +0545

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -114,6 +114,7 @@ app.use((req, res, next) => {
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'frontend/build')));
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
